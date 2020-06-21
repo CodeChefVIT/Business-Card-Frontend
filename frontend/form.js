91968f204f529063bd63e05fbd6f0b99b4035066
NavyaaSharma <navyaasharma700@rediffmail.com>
2020-06-21 22:18:37 +0530

    form fixes

--- frontend/form.js
+++ frontend/form.js
@@ -1,11 +1,23 @@
 
 function form()
 {
+    //wrong format
+    
+    // var data={
+    //     name:document.getElementById('firstName').value+document.getElementById('lastName').value,
+    //     company:document.getElementById('companyName').value,
+    //     github:document.getElementById('githuburl').value,
+    //     linkedin:document.getElementById('linkedinurl').value
+    // }
+    
+
+    //refer the docs and then write the object
     var data={
-        name:document.getElementById('firstName').value+document.getElementById('lastName').value,
-        company:document.getElementById('companyName').value,
-        github:document.getElementById('githuburl').value,
-        linkedin:document.getElementById('linkedinurl').value
+        "firstName": document.getElementById('firstName').value,
+        "lastName": document.getElementById('lastName').value,
+        "companyName": document.getElementById('companyName').value,
+        "githubUrl": document.getElementById('githuburl').value,
+        "linkedinUrl": document.getElementById('linkedinurl').value
     }
     var xh = new XMLHttpRequest();
     xh.open("POST", "https://business-card-api-rs.herokuapp.com/api/users/add", true)
